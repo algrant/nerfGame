93c63ea70a61c67911f3cf8e78d85ee4b61b72b3
Al Grant <al@intergalactic.com>
2017-02-05 18:43:37 -0800

    Cleanup unused

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,29 @@
-// #include "Arduino.h"
-//
-// int buttonPin = 9;
-// int ledPin = 13;
-// int ledDelay = 1000;
-// bool ledBlinkState = HIGH;
-// long ledTimer;
-// bool buttonPressed;
-// bool ledState;
-//
-// void checkBlinkTimer() {
-//   if (millis() - ledTimer > ledDelay) {
-//     ledTimer = millis();
-//     ledBlinkState = !ledBlinkState;
-//   }
-// }
-//
-// void checkButtonPress() {
-//   buttonPressed = (digitalRead(buttonPin) == HIGH);
-// }
-//
-// void determineLedState() {
-//   if (ledBlinkState || buttonPressed) {
-//     ledState = HIGH;
-//   } else {
-//     ledState = LOW;
-//   }
-// }
-//
-// void display() {
-//   digitalWrite(ledPin, ledState);
-// }
-//
-// void determineState() {
-//   checkBlinkTimer();
-//   checkButtonPress();
-//   determineLedState();
-// }
-//
-//
-// // Run first on Arduino
-// void setup() {
-//   pinMode(buttonPin, INPUT);
-//   pinMode(ledPin, OUTPUT);
-//
-//   ledTimer = millis();
-// }
-//
-// // Arduino main loop
-// void loop() {
-//   determineState();
-//   display();
-// }
+#include "FastLED.h"
+#include <BlinkenLight.h>
+#include <Target.h>
+
+#include "Arduino.h"
+
+BlinkenLight target(13, 1000, 1000);
+
+CRGB leds[22];
+
+Target colourTarget(leds, 0, 5, 1000, 1000);
+Target colourTarget2(leds, 5, 5, 1500, 1500);
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, 6>(leds, 22);
+  Serial.begin(9600);
+}
+void loop() {
+  // static uint8_t hue = 0;
+  // FastLED.showColor(CHSV(hue++, 255, 55));
+  // delay(100);
+  target.Update();
+  colourTarget.Update();
+  colourTarget2.Update();
+  if (millis() % 500 == 0){
+    Serial.print(leds[1]);
+  }
+  FastLED.show();
+}
